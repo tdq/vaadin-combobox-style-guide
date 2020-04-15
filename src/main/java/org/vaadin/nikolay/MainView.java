cd2977ac5b199a886963275864936a991e62ba1e
Nikolay <xxtdqxx@gmail.com>
2020-04-15 10:18:22 +0000

    clean up

--- src/main/java/org/vaadin/nikolay/MainView.java
+++ src/main/java/org/vaadin/nikolay/MainView.java
@@ -1,15 +1,10 @@
 package org.vaadin.nikolay;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 /**
